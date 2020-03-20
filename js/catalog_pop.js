f389e8c6398289850fd26a76303451c201215412
bumam <m.popleteeva@gmail.com>
2020-03-20 18:41:49 +0300

    finish10

--- js/catalog_pop.js
+++ js/catalog_pop.js
@@ -1,20 +1,22 @@
 var link = document.querySelectorAll(".buy_popup");
-   var popup = document.querySelector(".modul_catalog");
-   var close = popup.querySelector(".modul_close");
-   
-   for (let lin of link)  {
-   lin.addEventListener("click", function (event) {
-       event.preventDefault();
-       popup.classList.add("modul_close_show");
-   });
+var popup = document.querySelector(".modul_catalog");
+var close = popup.querySelector(".modul_close");
 
-   close.addEventListener("click", function (event) {
-       event.preventDefault();
-       popup.classList.remove("modul_close_show");
-   });}
-   window.addEventListener("keydown", function (event) {
-       if (event.keyCode === 27) {
-           if (popup.classList.contains("modul_close_show")) {
-               popup.classList.remove("modul_close_show");}
-       }
-   });
+for (let lin of link) {
+  lin.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.add("modul_close_show");
+  });
+
+  close.addEventListener("click", function (event) {
+    event.preventDefault();
+    popup.classList.remove("modul_close_show");
+  });
+}
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modul_close_show")) {
+      popup.classList.remove("modul_close_show");
+    }
+  }
+});
